8b71e416eb1aa21a5d49ca815ea70ef8d9d2386c
manazil <manazil.85888@gmail.com>
2025-08-18 04:15:59 +0530

    First version of hello package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/manazquamar/hello
+
+go 1.23.3
---
+++ hello.go
@@ -0,0 +1,6 @@
+package hello
+
+// SayHello returns a greeting message.
+func SayHello(name string) string {
+	return "Hello, " + name
+}
